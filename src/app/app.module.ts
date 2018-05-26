ff17ce5fc81578658544a32e21ad68cb39b62e9b
eastflag <eastflag@gmail.com>
2018-05-26 14:56:58 +0900

    라우팅 테이블 구성
    사용자 사이트 등 화면이 전혀 다를 경우 구성방법.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { HomeComponent } from './home/home.component';
+import { JqueryComponent } from './jquery/jquery.component';
+import { AngularComponent } from './angular/angular.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+  {path: '', component: IndexComponent, children: [
+      {path: '', component: HomeComponent},
+      {path: 'jquery', component: JqueryComponent},
+      {path: 'angular', component: AngularComponent},
+    ]}
+  // {path: 'admin', component: xxxx} 관리자 등 화면이 전혀 다른 사이트를 구현가능
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    HomeComponent,
+    JqueryComponent,
+    AngularComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
