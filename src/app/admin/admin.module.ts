9c5c2cf88fdcc8f20e800f9c798ceb56bcbc049f
eastflag <eastflag@gmail.com>
2018-06-02 16:48:15 +0900

    관리자 모듈 생성, 라우팅 설계

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
+import { IndexComponent } from './index/index.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+  {path: '', component: IndexComponent, children: [
+      {path: '', component: HomeComponent},
+      {path: 'news', component: NewsComponent}
+    ]}
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [HomeComponent, NewsComponent, IndexComponent]
+})
+export class AdminModule { }
