bac9222bcb5f31b5b96a78018aad27d3edade756
eastflag <eastflag@gmail.com>
2018-06-02 11:40:17 +0900

    할일 추가하기 - form 유효성 검사

--- src/app/angular/angular.component.ts
+++ src/app/angular/angular.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from "../user.service";
 import {TodoVO} from "../domain/todo.vo";
+import {Form} from "@angular/forms";
 
 @Component({
   selector: 'app-angular',
@@ -25,7 +26,7 @@ export class AngularComponent implements OnInit {
       });
   }
 
-  addTodo() {
+  addTodo(myForm: Form) {
     console.log('add todo');
     const todo = new TodoVO();
     todo.todo = this.newTodo;
@@ -34,6 +35,8 @@ export class AngularComponent implements OnInit {
       .subscribe(body => {
         // todoList 맨 앞에 데이터를 추가한다.
         this.todoList.unshift(body);
+        // 입력된 값 삭제
+        this.newTodo = null;
       });
   }
 }
