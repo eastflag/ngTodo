d4ec77a592b07dc479b10190242ccb8c9a70493c
eastflag <eastflag@gmail.com>
2018-06-02 17:07:56 +0900

    관리자화면 구성
    사이드 메뉴 - expantion panel
    flexlayout의 반응형 api로 모바일 화면구성

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,6 +4,20 @@ import { HomeComponent } from './home/home.component';
 import { NewsComponent } from './news/news.component';
 import { IndexComponent } from './index/index.component';
 import {RouterModule, Routes} from "@angular/router";
+import {
+  MatButtonModule,
+  MatCardModule, MatExpansionModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatMenuModule,
+  MatToolbarModule
+} from "@angular/material";
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {FormsModule} from "@angular/forms";
 
 const routes: Routes = [
   {path: '', component: IndexComponent, children: [
@@ -15,7 +29,18 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    FormsModule,
+    HttpClientModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatExpansionModule,
   ],
   declarations: [HomeComponent, NewsComponent, IndexComponent]
 })
