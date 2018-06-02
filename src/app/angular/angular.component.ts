bc33ca4eaee7769c1a32a14b40cf9b106e6c2b4f
eastflag <eastflag@gmail.com>
2018-06-02 12:04:07 +0900

    할일 추가 - 애니메이션 구현

--- src/app/angular/angular.component.ts
+++ src/app/angular/angular.component.ts
@@ -2,11 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import {UserService} from "../user.service";
 import {TodoVO} from "../domain/todo.vo";
 import {Form} from "@angular/forms";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-angular',
   templateUrl: './angular.component.html',
-  styleUrls: ['./angular.component.css']
+  styleUrls: ['./angular.component.css'],
+  animations: [
+    trigger('flyInOut', [
+      // 글로벌 상태 정의
+      state('in', style({opacity: 1, transform: 'translate(0, 0)'})),
+      // transition 정의: A => B, A상태, animate(시간, B상태)
+      transition('void => in', [
+        style({opacity: 0, transform: 'translate(-100%, 0)'}),
+        animate(300)
+      ]),
+      transition('* => void', [
+        animate(300, style({transform: 'translate(0, -100%)', opacity: '0'}))
+      ])
+    ])
+  ]
+
 })
 export class AngularComponent implements OnInit {
   todoList: TodoVO[] = [];
@@ -26,7 +42,7 @@ export class AngularComponent implements OnInit {
       });
   }
 
-  addTodo(myForm: Form) {
+  addTodo() {
     console.log('add todo');
     const todo = new TodoVO();
     todo.todo = this.newTodo;
